fe7a0e1c860b7cb7d8dc5a366744f681fabcf58a
sharkLoc <50580507+sharkLoc@users.noreply.github.com>
2021-06-09 16:22:23 +0800

    Update skr.h

--- skr.h
+++ skr.h
@@ -1,13 +1,34 @@
-#ifndef _H_SEQ
-#define _H_SEQ
+#ifndef _H_SEQ_
+#define _H_SEQ_
 
 #include <zlib.h>
+#include <stdint.h>
+
+#define Q20 20
+#define Q30 30
+
+typedef struct Stat {
+	float averageLen,gc;
+	uint64_t readCount,baseCount,nCount,aCount,tCount,gCount,cCount,q20,q30;
+} stat;
+
+typedef struct readInfo {
+	char *line[4];
+	size_t len;
+} rinfo;
+
 
 char *readline(gzFile file);
 
 void mergeVcfs(int argc, char *argv[]);
 
 void fq2fa(int argc, char *argv[]);
+
+uint64_t qualityN(char *line,size_t q);
+
+void freeRead(rinfo read);
+
+void fqstat(int argc, char *argv[]);
 
 void statVcfs(int argc, char *argv[]);
 
